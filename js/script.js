afe92835766164c27e1cc2f2366ba6015c6cc3ff
karthikeyanpalaniswamy <karthikeyan.forethought@gmail.com>
2018-01-15 16:51:40 +0100

    Adapt tab click so that clicking on "Paid monthly" and "Paid yearly" show the respective price per month

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 (function(){
 
-    var ACTIVE_MENU_CLASS = "active_plan";
-    var $plan_btns = $('.pricing_plan--btn');
+    var ACTIVE_MENU_CLASS = "active_tab";
+    var $plan_tabs = $('.pricing_plans_tabs--tab');
     var $pricing_wrapper = $('.pricing_wrapper');
+    var YEARLY_PLAN_CLASS = 'pricing_yearly_plan';
 
-    $plan_btns.click(function(){
+    $plan_tabs.click(function(){
+        if ($(this).hasClass(ACTIVE_MENU_CLASS)) {
+            return;
+        }
         $('.' + ACTIVE_MENU_CLASS).removeClass(ACTIVE_MENU_CLASS);
-        $(this).addClass('active_plan');
+        $(this).addClass(ACTIVE_MENU_CLASS);
 
-        if ($pricing_wrapper.hasClass('pricing_yearly_plan')) {
-            $pricing_wrapper.removeClass('pricing_yearly_plan');
+        if ($pricing_wrapper.hasClass(YEARLY_PLAN_CLASS)) {
+            $pricing_wrapper.removeClass(YEARLY_PLAN_CLASS);
         }
         else {
-            $pricing_wrapper.addClass('pricing_yearly_plan');
+            $pricing_wrapper.addClass(YEARLY_PLAN_CLASS);
         }
 
     });
