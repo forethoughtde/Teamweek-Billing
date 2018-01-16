00d8a5ee589098002c1b685467929300df5674e4
karthikeyanpalaniswamy <karthikeyan.forethought@gmail.com>
2018-01-16 12:28:21 +0100

    Add payment_modal.js file to open the payment modal.

---
+++ js/payment_modal.js
@@ -0,0 +1,11 @@
+(function(){
+
+    var $payment_modal_open = $('.payment_modal_open');
+    var $payment_modal = $('.pricing_modal');
+
+    $payment_modal_open.click(function(){
+        $payment_modal.fadeIn();
+    });
+
+
+})();
